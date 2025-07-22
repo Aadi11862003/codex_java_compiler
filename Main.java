d4e01311df8b29352224b601a1a71a47f019f5e8
Aadi11862003 <aadibharadwaj3@gmail.com>
2025-07-22 18:32:28 +0530

    add

--- Main.java
+++ Main.java
@@ -1,14 +1,5 @@
 public class Main {
-   
-    public static String reverse(String str) {
-        if (str.isEmpty()) return str;  // base case
-        return reverse(str.substring(1)) + str.charAt(0);  // recursive case
-    }
-
     public static void main(String[] args) {
-        String input = "hello";
-        System.out.println("Reversed string: " + reverse(input));
+        System.out.println("Hello, World!");
     }
-
-
 }
